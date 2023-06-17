9c21946ac7c4ae4eed1f399376da39fe2d7a7df5
Aster255 <paaalcos@gmail.com>
2023-06-17 11:13:44 +0800

    header component

---
+++ src/Components/Title.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "../images/cato.webp";
+import "../css/Title.css";
+
+function Title() {
+  return (
+    <div className="Title">
+      <div>
+        <h1>SFPOPOS</h1>
+        <div className="Title-Subtitle">
+          San Franciscos Privately Owned Public Spaces
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Title;
